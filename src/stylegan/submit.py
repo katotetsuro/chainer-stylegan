56c26774424770f8718b8fa7dd0a62728dd094f7
katotetsuro <nistetsurooy@gmail.com>
2019-07-19 18:16:24 +0900

    use truncate but score got worse

--- src/stylegan/submit.py
+++ src/stylegan/submit.py
@@ -5,16 +5,32 @@ from PIL import Image
 import os
 
 def create_submit_data(mapping, gen, smooth):
-    z = zipfile.PyZipFile('images_ns.zip' if not smooth else 'images.zip', mode='w')
     xp = gen.xp
     batchsize = 100
     num_output = 10000
     assert num_output % batchsize == 0
 
+    # w_batch_size = 100
+    # n_avg_w = 20000
+
+    # n_batches = n_avg_w // w_batch_size
+    # w_avg = xp.zeros(256).astype('f')
+    # with chainer.using_config('train', False), chainer.using_config('enable_backprop', False):
+    #     for i in range(n_batches):
+    #         z = chainer.Variable(xp.asarray(mapping.make_hidden(w_batch_size)))
+    #         w_cur = mapping(z)
+    #         w_avg = w_avg + xp.average(w_cur.data, axis=0)
+    # w_avg = w_avg / n_batches
+
+    # trc_psi = 0.0
+    z = zipfile.PyZipFile('images_ns.zip' if not smooth else 'images.zip', mode='w')
     for i in range(num_output // batchsize):
-        h = chainer.Variable(xp.asarray(mapping.make_hidden(batchsize)))
         with chainer.using_config('train', False), chainer.using_config('enable_backprop', False):
-            x = gen(mapping(h), stage=8)
+            w = chainer.Variable(xp.asarray(mapping.make_hidden(batchsize)))
+            w = mapping(w).array
+            #delta = w - w_avg
+            #w = delta * trc_psi + w_avg
+            x = gen(mapping(w), stage=8)
             x = (x + 1) * 127.5
         x = chainer.cuda.to_cpu(x.array)
         x = xp.clip(x, 0.0, 255.0)
